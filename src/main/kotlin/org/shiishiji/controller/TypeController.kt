4417364025f0585f2e69b7acb157f4ad18c0538b
Siguard <dszopinski97@gmail.com>
2020-08-14 22:48:23 +0200

    Changed response type of getTypes method in TypesController to SuccessResponse.

--- src/main/kotlin/org/shiishiji/controller/TypeController.kt
+++ src/main/kotlin/org/shiishiji/controller/TypeController.kt
@@ -7,6 +7,7 @@ import io.javalin.plugin.openapi.annotations.OpenApiResponse
 import me.liuwj.ktorm.database.Database
 import me.liuwj.ktorm.dsl.*
 import org.shiishiji.Connection
+import org.shiishiji.controller.response.SuccessResponse
 import org.shiishiji.enitity.Type
 import org.shiishiji.model.Types
 
@@ -18,15 +19,18 @@ object TypeController {
         summary = "Get all card types",
         operationId = "getTypes",
         tags = ["type"],
-        responses = [OpenApiResponse("200", [OpenApiContent(Array<Type>::class)])]
+        responses = [OpenApiResponse("200", [OpenApiContent(SuccessResponse::class)])]
     )
     fun getTypes(ctx: Context) {
         val types = getDatabase().from(Types)
-                            .select()
-                            .orderBy(Types.value.asc())
-                            .map { row -> Types.createEntity(row).toHashMap() }
+            .select()
+            .orderBy(Types.value.asc())
+            .map { row -> Types.createEntity(row).toHashMap() }
 
-        ctx.json(types.toList())
+        val data = HashMap<String, Any>()
+        data["rows"] = types
+
+        ctx.json(SuccessResponse(message = "Returned types.", data = data))
     }
 
 
